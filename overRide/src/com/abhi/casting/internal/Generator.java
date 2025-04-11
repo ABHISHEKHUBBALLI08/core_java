81b6fca007aebbeefae3222f9330b9b734718af0
View ABHISHEKHUBBALLI08 <abhishekhubballi0806@gmail.com>
2025-04-11 23:22:04 +0530

    task 11-04-2025-2

---
+++ overRide/src/com/abhi/casting/internal/Generator.java
@@ -0,0 +1,10 @@
+package com.abhi.casting.internal;
+
+public class Generator {
+    public  Generator(){
+        System.out.println("no arg constructor in generator ");
+    }
+    public void current(){
+        System.out.println("current running in generator");
+    }
+}
